86f20866ff0180979592e161ca07af0cf904b189
psh <pariente@naver.com>
2024-01-09 23:52:37 +0900

    로그인 화면 UI 조금 개발_240109

--- community/src/main/java/com/saeahga/community/controller/UserController.java
+++ community/src/main/java/com/saeahga/community/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,5 +24,15 @@ public class UserController {
         resultMap = userService.getUser(userId);
 
         return resultMap;
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        ModelAndView mv = new ModelAndView();
+
+        // templates 폴더 안에서, user 폴더 아래 login.html
+        mv.setViewName("user/login");
+
+        return mv;
     }
 }
